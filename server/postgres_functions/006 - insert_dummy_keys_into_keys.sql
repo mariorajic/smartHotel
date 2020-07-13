66d2d003837babfee1ff93f3f9e641d9c1acf4c2
mariorajic <mariorajic7@gmail.com>
2020-07-13 14:08:28 +0200

    more initial keys for dummy data

--- server/postgres_functions/006 - insert_dummy_keys_into_keys.sql
+++ server/postgres_functions/006 - insert_dummy_keys_into_keys.sql
@@ -6,3 +6,13 @@ VALUES
 ('123abc456', false, NOW(), NOW()),
 ('abc123456', false, NOW(), NOW()),
 ('123456abc', false, NOW(), NOW());
+('1234abcde', false, NOW(), NOW()),
+('12345abcd', false, NOW(), NOW()),
+('123456abc', false, NOW(), NOW()),
+('1234567ab', false, NOW(), NOW()),
+('12345678b', false, NOW(), NOW()),
+('9876abcde', false, NOW(), NOW()),
+('98765abcd', false, NOW(), NOW()),
+('987654abc', false, NOW(), NOW()),
+('9876543ab', false, NOW(), NOW()),
+('98765432b', false, NOW(), NOW());
